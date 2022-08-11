b144efae68d38b87c743bc0735e5467cedbc9e8b
Kheem Chandra <kheemchandrasingh@gmail.com>
2022-08-11 09:18:13 +0530

    use REACT TRANSITION GROUP for smooth transition

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component } from "react"; 
 
 import "./App.css";
 import Modal from "./components/Modal/Modal";
@@ -6,13 +6,29 @@ import Backdrop from "./components/Backdrop/Backdrop";
 import List from "./components/List/List";
 
 class App extends Component {
+  constructor(props){
+    super(props);
+  
+  this.state = {
+    modalIsShown: false
+  } 
+}
+  modalOpen = () => { 
+    this.setState({modalIsShown: true})
+  }
+
+  modalClosed = () => { 
+    this.setState({modalIsShown: false})
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>React Animations</h1>
-        <Modal />
-        <Backdrop />
-        <button className="Button">Open Modal</button>
+        <h1>React Animations</h1> 
+        <Modal show={this.state.modalIsShown} closed={this.modalClosed}/> 
+         
+        { this.state.modalIsShown ?  <Backdrop /> : null}        
+        <button onClick={this.modalOpen} className="Button">Open Modal</button>
         <h3>Animating Lists</h3>
         <List />
       </div>
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React from "react"; 
+import CSSTransition from "react-transition-group/CSSTransition";
 
-import './Modal.css';
+import "./Modal.css";
 
-const modal = (props) => (
-    <div className="Modal">
+const animatingTime = {
+  enter: 400,
+  exit: 1000,
+};
+
+const modal = (props) => {
+  // use Transition with different state
+  return (
+    <CSSTransition
+      in={props.show}
+      timeout={animatingTime}
+      mountOnEnter
+      unmountOnExit
+      classNames={{
+        enter: '',
+        enterActive: 'ModalOpen',
+        exit: '',
+        exitActive: 'ModalClosed'
+      }}
+    >
+      <div className="Modal">
         <h1>A Modal</h1>
-        <button className="Button" onClick={props.closed}>Dismiss</button>
-    </div>
-);
+        <button className="Button" onClick={props.closed}>
+          Dismiss
+        </button>
+      </div>
+    </CSSTransition>
+  );
+};
+
 
 export default modal;
